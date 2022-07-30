6c3104d91bfe85c98004c489c549dafeaaccbe90
KC-2001MS <79145483+KC-2001MS@users.noreply.github.com>
2022-07-30 21:40:03 +0900

    課題のあるコードの追加

--- Sources/OnboardingUI/PreView.swift
+++ Sources/OnboardingUI/PreView.swift
@@ -1,5 +1,5 @@
 //
-//  FirstLaunchView.swift
+//  PreView.swift
 //  Simple Editor X (macOS)
 //
 //  Created by 茅根啓介 on 2022/04/13.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-//初期起動画面
+//フレームワーク使用例
 @available(iOS 14.0,macOS 11,*)
 struct FirstLaunchView: View {
     var body: some View {
@@ -73,6 +73,66 @@ struct FirstLaunchView: View {
                 ContinueButton(action:{
                     
                 })
+                Spacer()
+                    .frame(height: geom.size.height/14.5)
+            }
+        }
+#endif
+    }
+}
+
+//うまくいかないフレームワーク実装
+@available(iOS 14.0,macOS 11,*)
+struct OnboardingView<Title: View,Content: View,Button: View>: View {
+    let title: Title
+    let content: Content
+    let button: Button
+    
+    init(@ViewBuilder title: () -> Title,
+         @ViewBuilder content: () -> Content,
+         @ViewBuilder button: () -> Button) {
+        self.title = title()
+        self.content = content()
+        self.button = button()
+    }
+    
+    var body: some View {
+#if os(OSX)
+        VStack {
+            Group {
+                Spacer()
+                    .frame(height: 50)
+                title
+                Spacer()
+                    .frame(height: 50)
+                VStack(alignment: .leading, spacing: 40) {
+                    content
+                }
+            }
+            
+            Spacer()
+                .frame(height: 70)
+            
+            button
+            
+            Spacer()
+                .frame(height: 30)
+        }
+#elseif os(iOS)
+        GeometryReader { geom in
+            VStack {
+                ScrollView {
+                    Spacer()
+                        .frame(height: geom.size.height/9.5)
+                    title
+                    Spacer()
+                        .frame(height: geom.size.height/14.5)
+                    VStack(alignment: .leading, spacing: 40) {
+                        content
+                    }
+                }
+                Spacer()
+                button
                 Spacer()
                     .frame(height: geom.size.height/14.5)
             }
