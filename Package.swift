a0a5eb3a8e1ed94581730de86f6b8f9d975558b4
KC-2001MS <79145483+KC-2001MS@users.noreply.github.com>
2022-08-02 18:55:52 +0900

    バージョン情報の追加

--- Package.swift
+++ Package.swift
@@ -5,8 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "OnboardingUI",
-    
-    platforms: [.iOS(.v14),.macOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
