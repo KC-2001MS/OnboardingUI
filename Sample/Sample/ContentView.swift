de036e35d69e04636f734faa20204373c9a9a690
KC-2001MS <iroiro.work1234@gmail.com>
2023-12-08 22:43:01 +0900

    サンプルアプリの追加

---
+++ Sample/Sample/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Sample
+//  
+//  Created by Keisuke Chinone on 2023/12/08.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sample/Sample/SampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  SampleApp.swift
+//  Sample
+//  
+//  Created by Keisuke Chinone on 2023/12/08.
+//
+
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
